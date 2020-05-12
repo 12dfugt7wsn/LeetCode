163c721a37b741f1fe3672a9d787e517fbd4df36
Lucia Lu <43317487+12dfugt7wsn@users.noreply.github.com>
2020-05-12 10:56:51 -0500

    Update may12.py

--- may12.py
+++ may12.py
@@ -35,3 +35,25 @@ def isMatch(self, s: str, p: str) -> bool:
                 else:
                     dp[i][j] = dp[i-1][j-1] and s[i-1] == p[j-1]
         return dp[len(s)][len(p)]
+
+def intToRoman(self, num: int) -> str:
+        val = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1
+            ]
+        syb = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+            ]
+        roman_num = ''
+        i = 0
+        while  num > 0:
+            for _ in range(num // val[i]):
+                roman_num += syb[i]
+                num -= val[i]
+            i += 1
+        return roman_num

b34e5c14ac63c38645fc7592c045702514aef54e
Lucia Lu <43317487+12dfugt7wsn@users.noreply.github.com>
2020-05-13 12:21:58 -0500

    Update may13.py

--- may13.py
+++ may13.py
@@ -19,3 +19,38 @@ def romanToInt( s: str) -> int:
     return result 
 
 print(romanToInt("MCMXCIV"))
+
+def longestCommonPrefix(self, strs: [str]) -> str:
+    res, i, j = '', 0, 0
+    try:
+        while True:
+            if i == len(strs) - 1:
+                res += strs[0][j]
+                i = 0
+                j += 1
+            if strs[i][j] == strs[i+1][j]:
+                i += 1
+            else:
+                return res
+    except:
+        return res
+
+def threesum(self, nums: [int]):
+    result = set()
+    mapping = {}
+    geq0 = set()
+    leq0 = set()
+    for i in nums:
+        mapping[i] = 1 if i not in mapping.keys() else mapping[i] + 1
+        if i >= 0:
+            geq0.add(i)
+        if i <= 0:
+            leq0.add(i)
+    
+    for a in geq0:
+        for b in leq0:
+            c = - a - b
+            potential = [a, b, c]
+            if c in mapping.keys() and mapping[c] >= sum([x == c for x in potential]):
+                result.add(tuple(sorted(potential)))
+    return result
